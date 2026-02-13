c6e990d132baf0fc5d2fcd5291151bcb4c34363b
Sri-kanth-J <srikanthj23ad@psnacet.edu.in>
2026-02-13 15:44:16 +0530

    Update .gitignore: exclude comments and MLDemoProj folders

--- comments/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility module for skin disease classification project.
-
-Contains helper functions and classes for:
-- Image preprocessing and loading
-- Class weight computation
-- Data visualization
-- Model utilities
-"""
